fd2e759dcf24e7d187b177da02090f48bed5b3bc
Bayfrosch <bayfrosch@gmx.de>
2026-01-12 21:11:56 +0100

    Hinzufügen/Löschen von Items

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,19 @@ function App() {
     { id: 3, name: "Eier", completed: false }
   ]);
 
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = () => {
+    if (inputValue.trim() === "") return;
+    const newItem: ShoppingItemData = {
+      id: items.length + 1,
+      name: inputValue,
+      completed: false
+    };
+    setItems([...items, newItem]);
+    setInputValue("");
+  };
+
   const handleToggle = (id: number) => {
     setItems(
       items.map(item => {
@@ -25,19 +38,36 @@ function App() {
       })
     );
   };
-
+  const handleDelete = (id: number) => {
+    setItems(items.filter(item => item.id !== id));
+  };
+  
   return (
     <div>
-      <h1>Shopping List</h1>
-      {items.map(item => (
-        <ShoppingItem 
-          key={item.id}
-          id={item.id}
-          name={item.name}
-          completed={item.completed}
-          onToggle={() => handleToggle(item.id)}
+      <div>
+        <h1>Shopping List</h1>
+        {items.map(item => (
+          <ShoppingItem 
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            completed={item.completed}
+            onToggle={() => handleToggle(item.id)}
+            onDelete={() => handleDelete(item.id)}
+          />
+        ))}
+      </div>
+      <div>
+        <input 
+          type="text" 
+          value={inputValue} 
+          onChange={(e) => setInputValue(e.target.value)} 
+          placeholder="Was brauchst du?"
         />
-      ))}
+        <br/>
+        <button onClick={handleSubmit}>Hinzufügen</button>
+        <button onClick={() => setItems([])}>Liste leeren</button>
+      </div>
     </div>
   );
 }
--- src/components/ShoppingItem.tsx
+++ src/components/ShoppingItem.tsx
@@ -5,6 +5,7 @@ interface ShoppingItemProps {
     name: string,
     completed: boolean,
     onToggle: () => void;
+    onDelete: () => void;
 }
 
 function ShoppingItem(props: ShoppingItemProps) {
@@ -12,8 +13,12 @@ function ShoppingItem(props: ShoppingItemProps) {
         <div 
         className={props.completed ? "shopping-item checked" : "shopping-item" }
         onClick={() => props.onToggle()}
-        >            
+        >
             <h3>{props.name}</h3>
+            <button onClick={(e) => {
+                e.stopPropagation();
+                props.onDelete();
+            }}>X</button>
         </div>
     )
 }
